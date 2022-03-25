a4032be6e2ef78c1905104e8ce195a79a0ad165e
Andrey Spiridonov <andrejvazapkin@gmail.com>
2022-03-25 14:04:24 +0400

    Added tests

--- src/test/java/ru/netology/manager/ManagerTest.java
+++ src/test/java/ru/netology/manager/ManagerTest.java
@@ -1,5 +1,6 @@
 package ru.netology.manager;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
@@ -13,23 +14,28 @@ class ManagerTest {
     Book book = new Book(1, "Kolobok", 100, "ChackPalanik");
     Smartphone smartphone = new Smartphone(2, "GalaxyA6", 20000, "Samsung");
     Product tshort = new Product(3, "TShort", 200);
+    Smartphone smartphone1 = new Smartphone(4, "GalaxyS6", 25000, "Samsung");
 
-    @Test
-    void shouldSearchByProducer() {
+
+    @BeforeEach
+    public void initEach() {
         man.add(book);
         man.add(smartphone);
         man.add(tshort);
+        man.add(smartphone1);
+    }
+
+    @Test
+    void shouldSearchByProducer() {
 
         Product[] expected = {smartphone};
         Product[] actual = man.searchBy("Samsung");
         assertArrayEquals(expected, actual);
     }
 
+
     @Test
     void shouldSearchByName() {
-        man.add(book);
-        man.add(smartphone);
-        man.add(tshort);
 
         Product[] expected = {book};
         Product[] actual = man.searchBy("Kolobok");
@@ -38,12 +44,25 @@ class ManagerTest {
 
     @Test
     void shouldSearchByAuthor() {
-        man.add(book);
-        man.add(smartphone);
-        man.add(tshort);
 
         Product[] expected = {book};
         Product[] actual = man.searchBy("ChackPalanik");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSearchMoreThenOne() {
+
+        Product[] expected = {smartphone, smartphone1};
+        Product[] actual = man.searchBy("Samsung");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSearchByNonexistentName() {
+
+        Product[] expected = {};
+        Product[] actual = man.searchBy("IPhone");
         assertArrayEquals(expected, actual);
     }
 }
